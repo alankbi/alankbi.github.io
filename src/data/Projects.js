ca63adb0992822596f45e38ba82be8ef19b0073d
Alan Bi <alan.bi326@gmail.com>
2022-02-10 23:08:59 -0500

    Remove old projects and make small wording changes

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -2,7 +2,7 @@ export default {
   microsoft: {
     title: 'Microsoft',
     description: 'SWE intern on the Intune UX team developing device barcode scanning functionality. ',
-    longdescription: 'I spent the summer of 2021 as a remote SWE intern at Microsoft, adding a new ' +
+    longdescription: 'I spent the summer of 2021 as a remote SWE intern at Microsoft adding a new ' +
         'device barcode scanning functionality to Intune. This service aims to help educators better ' +
         'keep track of and manage their school-issued devices by enabling bulk scan and administer ' +
         'capabilities on dozens of devices at a time. ',
@@ -26,8 +26,8 @@ export default {
     title: 'HackDuke',
     description: 'Co-director of the nation\'s premier tech + social good hackathon.',
     longdescription: 'After joining the sponsorship team in fall 2019 and becoming sponsorship co-lead ' +
-        'in spring 2020, I served as co-director of HackDuke for the 2021 calendar year. I helped lead a ' +
-        'team of 25+ students to organize Duke University\'s largest hackathon and design conference with ' +
+        'in spring 2020, I served as co-director of HackDuke for the 2021 calendar year. I led a team ' +
+        'of 25+ students to organize Duke University\'s largest hackathon and design conference with ' +
         'over $40k raised and 500 participants at our events.',
     image: '/assets/images/project/hackduke.png',
     link: 'https://hackduke.org/',
@@ -61,25 +61,18 @@ export default {
   fidelity: {
     title: 'Fidelity',
     description: 'Interned on the trading platform team building services with Java Spring and React.',
-    longdescription: 'I was a SWE intern on Fidelity\'s trading platform team, where I worked remotely ' +
-        'with two other interns on an order monitoring system. This system allows engineers to monitor ' +
-        'and direct trading traffic between multiple sets of production environments up to several ' +
-        'hundred thousand orders a day.',
+    longdescription: 'I was a SWE intern in 2020 on Fidelity\'s trading platform team, where I worked ' +
+        'remotely with two other interns on an order monitoring system. This system allows engineers to ' +
+        'monitor and direct trading traffic between multiple sets of production environments up to ' +
+        'several hundred thousand orders a day.',
     image: '/assets/images/project/fidelity.png',
     link: 'http://fidelity.com/',
     tags: ['work', 'intern', 'java', 'react'],
   },
-  dukeml: {
-    title: 'Duke ML',
-    description: 'Organizing large-scale machine learning events for the student body.',
-    image: '/assets/images/project/dml.png',
-    link: 'https://dukeml.org/',
-    tags: ['work', 'leadership', 'machine-learning'],
-  },
-  'uw-2019': {
+  'uw-cse': {
     title: 'UW CSE',
     description: 'Research intern using machine learning to improve mutation testing.',
-    longdescription: 'The summer before college, I worked with a professor at the University of ' +
+    longdescription: 'The summer of 2019, I worked with a professor at the University of ' +
         'Washington to use active learning as a means of improving mutation testing, a type of ' +
         'analysis that evaluates the quality of existing software tests. Implementing active ' +
         'learning, as well as a grid search over 1800 hyperparameter combinations, helped increase ' +
@@ -112,78 +105,15 @@ export default {
     link: 'https://teamscode.org',
     tags: ['work', 'leadership', 'social-good'],
   },
-  statsify: {
-    title: 'Statsify',
-    description: 'Web application that visualizes interesting stats and info about websites.',
-    image: '/assets/images/project/statsify.png',
-    link: 'https://www.statsify.us',
-    tags: ['project', 'python', 'javascript', 'web', 'extension'],
-  },
-  'easy-due-dates': {
-    title: 'Easy Due Dates',
-    description: 'Trello power-up that pushes card due dates back by one day.',
-    image: '/assets/images/project/trello_easy_due_dates.png',
-    link: 'https://github.com/alankbi/trello-easy-due-dates',
-    tags: ['project', 'javascript', 'extension'],
-  },
   expedia: {
     title: 'Expedia Group',
     description: 'Interned on the Air Experience Team as a full-stack developer.',
-    longdescription: 'The summer after junior year, I was a SWE intern in Expedia\'s high school ' +
+    longdescription: 'The summer of 2018, I was a SWE intern in Expedia\'s high school ' +
         'apprenticeship program within their Bellevue and Seattle offices. Over the course of ' +
         'eight weeks, I helped implement 4 frontend stories, 3 backend stories, and 5 A/B tests ' +
-        'into production on the site\'s flight search and summary pages.',
+        'into production for the site\'s flight search functionality.',
     image: '/assets/images/project/expedia.png',
     link: 'https://docs.google.com/presentation/d/1ljYnmAM7By8gfDnNmaPnVGYLrkUY_q5yclxtgdL2i5g/edit?usp=sharing',
     tags: ['work', 'intern', 'java', 'javascript', 'web'],
-  },
-  'basic-ml': {
-    title: 'Basic ML',
-    description: 'Python package with implementations of common machine learning models.',
-    image: '/assets/images/project/ml_algorithms.png',
-    link: 'https://github.com/alankbi/basic-ml',
-    tags: ['project', 'python', 'machine-learning'],
-  },
-  'my-continents': {
-    title: 'My Continents',
-    description: 'Cross-platform mobile app made for Continents Academy.',
-    image: '/assets/images/project/continents.png',
-    link: 'https://github.com/alankbi/continents-app',
-    tags: ['work', 'c#', 'app'],
-  },
-  'bubble-game': {
-    title: 'Bubble Game',
-    description: 'Educational game made using Unity for Puppetry Arts.',
-    image: '/assets/images/project/bubble_game.png',
-    link: 'https://github.com/alankbi/bubble-game',
-    tags: ['work', 'c#', 'app'],
-  },
-  'gravity-sim': {
-    title: 'Gravity Sim',
-    description: 'A to-scale simulation of planets in a solar system.',
-    image: '/assets/images/project/gravity_simulation.svg',
-    link: 'https://github.com/alankbi/gravity-simulation',
-    tags: ['project', 'java', 'app'],
-  },
-  quicknews: {
-    title: 'QuickNews',
-    description: 'News app that clusters similar articles together for a quick, daily read.',
-    image: '/assets/images/project/quicknews.png',
-    link: 'https://github.com/alankbi/news-app',
-    tags: ['project', 'c#', 'app'],
-  },
-  'uw-2017': {
-    title: 'UW CSE',
-    description: 'Research intern helping with a Ph.D. student\'s project called Casper.',
-    image: '/assets/images/project/uwashington.png',
-    link: 'https://casper.uwplse.org',
-    tags: ['work', 'intern', 'php', 'java', 'big-data'],
-  },
-  tasklist: {
-    title: 'TaskList',
-    description: 'My first iOS app, a simple to-do list.',
-    image: '/assets/images/project/tasklist.png',
-    link: 'https://github.com/alankbi/TaskList',
-    tags: ['project', 'c#', 'app'],
   },
 };
