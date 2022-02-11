2acafba64f237d5268c46a44f9b8ce7c24295ffa
Alan Bi <alan.bi326@gmail.com>
2022-02-10 20:13:24 -0500

    Add new projects and long descriptions

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -1,14 +1,34 @@
 export default {
-  fidelity: {
-    title: 'Fidelity',
-    description: 'Interned on the trading platform team building services with Java Spring and React.',
-    image: '/assets/images/project/fidelity.png',
-    link: '/',
-    tags: ['work', 'intern', 'java', 'react'],
+  microsoft: {
+    title: 'Microsoft',
+    description: 'SWE intern on the Intune UX team developing device barcode scanning functionality. ',
+    longdescription: 'I spent the summer of 2021 as a remote SWE intern at Microsoft, adding a new ' +
+        'device barcode scanning functionality to Intune. This service aims to help educators better ' +
+        'keep track of and manage their school-issued devices by enabling bulk scan and administer ' +
+        'capabilities on dozens of devices at a time. ',
+    image: '/assets/images/project/microsoft.png',
+    link: 'https://microsoft.com/',
+    tags: ['work', 'intern', 'react', 'typescript', 'web'],
+  },
+  codejoust: {
+    title: 'CodeJoust',
+    description: 'An online code editing and problem-writing service for computer science educators.',
+    longdescription: 'CodeJoust is a year-long project that I worked on with several other students. It ' +
+        'is a fully-functioning online code editing and submission platform that allows users to compete ' +
+        'live against each other to solve programming problems. It is intended to be used by CS ' +
+        'educators, who can create custom problems and invite students to rooms for extra practice, ' +
+        'review, or just a fun way to end the class.',
+    image: '/assets/images/project/codejoust.png',
+    link: 'https://www.codejoust.co/',
+    tags: ['project', 'java', 'react', 'web'],
   },
   hackduke: {
     title: 'HackDuke',
-    description: 'Sponsorship lead for the nation\'s premier tech + social good hackathon.',
+    description: 'Co-director of the nation\'s premier tech + social good hackathon.',
+    longdescription: 'After joining the sponsorship team in fall 2019 and becoming sponsorship co-lead ' +
+        'in spring 2020, I served as co-director of HackDuke for the 2021 calendar year. I helped lead a ' +
+        'team of 25+ students to organize Duke University\'s largest hackathon and design conference with ' +
+        'over $40k raised and 500 participants at our events.',
     image: '/assets/images/project/hackduke.png',
     link: 'https://hackduke.org/',
     tags: ['work', 'leadership', 'social-good'],
@@ -19,11 +39,35 @@ export default {
     image: '/assets/images/project/detecto.svg',
     link: 'https://github.com/alankbi/detecto',
     tags: ['project', 'python', 'machine-learning'],
-    longdescription: 'Detecto is a Python package that allows you to build fully-functioning ' +
-      'computer vision and object detection models with just 5 lines of code. Inference on still ' +
-      'images and videos, transfer learning on custom datasets, and serialization of models to ' +
-      'files are just a few of Detecto\'s features. Detecto is built on top of PyTorch, allowing ' +
-      'an easy transfer of models between the two libraries.',
+    longdescription: 'Detecto is a Python package that allows users to build fully-functioning ' +
+        'object detection models with as little as 5 lines of code. After finding it difficult to ' +
+        'follow existing online tutorials on object detection, I decided to create this package ' +
+        'to streamline the model training and inference process as much as possible for newer ' +
+        'computer vision enthusiasts like myself. As of February 2022, Detecto has 530+ stars ' +
+        'on GitHub and 80k downloads.'
+  },
+  robotics: {
+    title: 'Duke Robotics',
+    description: 'Computer vision lead responsible for autonomous object detection capability.',
+    longdescription: 'I joined Duke Robotics my freshman year and served as the computer vision ' +
+        'lead my sophomore year. My key contribution was integrating object detection into the ' +
+        'robot, allowing it to autonomously detect and interact with objects in its surroundings. ' +
+        'I also helped explore stereo vision capabilities, which enables the robot to calculate ' +
+        'the 3D distances to such objects.',
+    image: '/assets/images/project/robotics.png',
+    link: 'https://duke-robotics.com/',
+    tags: ['work', 'leadership', 'python', 'machine-learning'],
+  },
+  fidelity: {
+    title: 'Fidelity',
+    description: 'Interned on the trading platform team building services with Java Spring and React.',
+    longdescription: 'I was a SWE intern on Fidelity\'s trading platform team, where I worked remotely ' +
+        'with two other interns on an order monitoring system. This system allows engineers to monitor ' +
+        'and direct trading traffic between multiple sets of production environments up to several ' +
+        'hundred thousand orders a day.',
+    image: '/assets/images/project/fidelity.png',
+    link: 'http://fidelity.com/',
+    tags: ['work', 'intern', 'java', 'react'],
   },
   dukeml: {
     title: 'Duke ML',
@@ -35,6 +79,11 @@ export default {
   'uw-2019': {
     title: 'UW CSE',
     description: 'Research intern using machine learning to improve mutation testing.',
+    longdescription: 'The summer before college, I worked with a professor at the University of ' +
+        'Washington to use active learning as a means of improving mutation testing, a type of ' +
+        'analysis that evaluates the quality of existing software tests. Implementing active ' +
+        'learning, as well as a grid search over 1800 hyperparameter combinations, helped increase ' +
+        'the efficiencies of a subset of machine learning models by up to 54%.',
     image: '/assets/images/project/uwashington.png',
     link: '/',
     tags: ['work', 'intern', 'python', 'machine-learning'],
@@ -42,18 +91,25 @@ export default {
   bidbar: {
     title: 'Bidbar',
     description: 'Mac app that allows users to save and run commands from the menu bar.',
+    longdescription: 'Bidbar is a Mac menu bar app that allows users to quickly save bash commands ' +
+        'and run them with global keyboard shortcuts. Based on personal experience, I felt that this ' +
+        'was a tool that could help developers speed up the execution of common scripts and keep ' +
+        'track of hard-to-remember commands. As of February 2022, 48 license keys have been sold for ' +
+        'Bidbar.',
     image: '/assets/images/project/bidbar.png',
     link: 'https://www.getbidbar.com',
     tags: ['project', 'swift', 'app'],
-    longdescription: 'Bidbar is a Mac app that allows you to save bash commands in the menu bar ' +
-      'and run/copy them with keyboard shortcuts. It\'s always tedious having to memorize and type out ' +
-      'lengthy, obscure bash commands. With Bidbar, they\'re all just a click of a button or key away!',
   },
   teamscode: {
     title: 'TeamsCode',
     description: 'Co-president of a 501(c)(3) nonprofit promoting CS to high schoolers.',
+    longdescription: 'In January of 2017, after noticing a lack of computer science-focused ' +
+        'opportunities within my community, I co-founded TeamsCode, a 501(c)(3) nonprofit that ' +
+        'organizes large-scale programming contests and provides online educational resources free ' +
+        'of cost. During my time as co-president, I helped organize nine in-person competitions ' +
+        'across two states, ultimately bringing together 700 participants across 50+ schools.',
     image: '/assets/images/project/teamscode.png',
-    link: 'https://teamscode.com',
+    link: 'https://teamscode.org',
     tags: ['work', 'leadership', 'social-good'],
   },
   statsify: {
@@ -73,6 +129,10 @@ export default {
   expedia: {
     title: 'Expedia Group',
     description: 'Interned on the Air Experience Team as a full-stack developer.',
+    longdescription: 'The summer after junior year, I was a SWE intern in Expedia\'s high school ' +
+        'apprenticeship program within their Bellevue and Seattle offices. Over the course of ' +
+        'eight weeks, I helped implement 4 frontend stories, 3 backend stories, and 5 A/B tests ' +
+        'into production on the site\'s flight search and summary pages.',
     image: '/assets/images/project/expedia.png',
     link: 'https://docs.google.com/presentation/d/1ljYnmAM7By8gfDnNmaPnVGYLrkUY_q5yclxtgdL2i5g/edit?usp=sharing',
     tags: ['work', 'intern', 'java', 'javascript', 'web'],
