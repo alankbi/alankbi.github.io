857c28731e5568343873ea33775f0fa9b49ed087
Alan Bi <alan.bi326@gmail.com>
2025-01-11 17:31:01 -0600

    Add lincoln park photo collection

--- src/data/PhotoCollections.js
+++ src/data/PhotoCollections.js
@@ -1,20 +1,20 @@
 export default {
-  '2024-10/lincoln-park': {
+  '2024-09/lincoln-park': {
     title: 'Lincoln Park',
     subtitle: 'Fall in Chicago. 2024',
-    imageFolder: '202409_test',
+    imageFolder: '202409_lincoln_park',
     content: [
-      'DSCF8621.jpg',
-      'DSCF8969.jpg',
-    ]
-  },
-  '2024-10/lincoln-test': {
-    title: 'Lincoln Park',
-    subtitle: 'Fall in Chicago. 2024',
-    imageFolder: '202409_test',
-    content: [
-      'DSCF8969.jpg',
-      'DSCF8621.jpg',
+      'DSCF8619.JPG',
+      'DSCF8621.JPG',
+      'DSCF8634.JPG',
+      'DSCF8642.JPG',
+      'DSCF8662.JPG',
+      'DSCF8676.JPG',
+      'DSCF8681.JPG',
+      'DSCF8690.JPG',
+      'DSCF8694.JPG',
+      'DSCF8698.JPG',
+      'DSCF8699.JPG',
     ]
   },
 };
