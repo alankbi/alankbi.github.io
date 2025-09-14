15f41d57d6ebeeb1c9a645b0ff644eb61ff9b287
Alan Bi <alan.bi326@gmail.com>
2025-09-14 17:12:33 -0500

    Add new photos

--- src/data/PhotoCollections.js
+++ src/data/PhotoCollections.js
@@ -1,7 +1,49 @@
 export default {
+  '2025-05/china': {
+    title: 'China',
+    subtitle: 'Visiting China for the first time in 15+ years.',
+    imageFolder: '202505_china',
+    content: [
+      'DSCF9488.jpg',
+      'IMG_9705.jpg',
+      'IMG_9707.jpg',
+      'DSCF9709.jpg',
+      'DSCF9732.jpg',
+      'DSCF9631.jpg',
+      'DSCF9517.jpg',
+      'DSCF9513.jpg',
+    ]
+  },
+  '2024-11/nyc': {
+    title: 'New York City',
+    subtitle: 'A collection of random photos taken across NYC.',
+    imageFolder: '202411_nyc',
+    content: [
+      'DSCF9147.jpg',
+      'DSCF9092.jpg',
+      'DSCF9062.jpg',
+      'DSCF9289.jpg',
+      'DSCF8912.jpg',
+      'DSCF9027.jpg',
+      'DSCF9030.jpg',
+    ]
+  },
+  '2024-10/milwaukee': {
+    title: 'Milwaukee',
+    subtitle: 'A cloudy, uneventful weekend in Milwaukee.',
+    imageFolder: '202410_milwaukee',
+    content: [
+      'DSCF8771.jpg',
+      'DSCF8730.jpg',
+      'DSCF8726.jpg',
+      'DSCF8736.jpg',
+      'DSCF8760.jpg',
+      'DSCF8762.jpg',
+    ]
+  },
   '2024-09/lincoln-park': {
     title: 'Lincoln Park',
-    subtitle: 'Fall in Chicago. 2024',
+    subtitle: 'Trying out a new camera for the first time in Chicago.',
     imageFolder: '202409_lincoln_park',
     content: [
       'DSCF8619.JPG',
